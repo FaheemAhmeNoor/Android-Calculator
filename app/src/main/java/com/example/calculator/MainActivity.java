b7f9ce299f2c115d586e039e12d906ddc44b3273
Faheem Ahmed Noor <faheemahmednoor4@gmail.com>
2021-10-23 20:29:25 -0700

    Addition Functionality added

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editTxt01, editTxt02;
     TextView textView;
     Button btnPlus, btnMinus, btnMul,btnDiv, btnPercentage, btnClear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
         btnDiv = findViewById(R.id.btnDiv);
         btnPercentage = findViewById(R.id.btnPercentage);
         btnClear = findViewById(R.id.btnClear);
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int firstValue, secondValue, ans;
+                firstValue = Integer.parseInt(editTxt01.getText().toString());
+                secondValue = Integer.parseInt(editTxt02.getText().toString());
+
+                ans = firstValue + secondValue;
+
+                textView.setText("Answer is " + ans);
+            }
+
+       btnMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int firstValue, secondValue, ans;
+                    firstValue = Integer.parseInt(editTxt01.getText().toString());
+                    secondValue = Integer.parseInt(editTxt02.getText().toString());
+
+                    ans = firstValue + secondValue;
+
+                    textView.setText("Answer is " + ans);
+                }
+
+        });
+
 
 
 
