05c451bf0901ebd5e31c1a59c5646a64f2c521b8
Faheem Ahmed Noor <faheemahmednoor4@gmail.com>
2021-10-23 20:01:01 -0700

    Id's of all ten views are found in mainActivity class

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTxt01, editTxt02;
+    TextView textView;
+    Button btnPlus, btnMinus, btnMul,btnDiv, btnPercentage, btnClear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTxt01 = findViewById(R.id.editTxt01);
+        editTxt02 = findViewById(R.id.editTxt02);
+
+        textView = findViewById(R.id.textView);
+
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMul = findViewById(R.id.btnMul);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnPercentage = findViewById(R.id.btnPercentage);
+        btnClear = findViewById(R.id.btnClear);
+
+
+
     }
 }
